07140708d609e292f5ec8d4121b2e13fa6eb121c
Ujjwal Rajput <ujjwalrajput8800@gmail.com>
2023-07-14 15:42:52 +0530

    1

--- cracker.py
+++ cracker.py
@@ -3,8 +3,6 @@ from tkinter import filedialog, messagebox
 import os
 import zipfile
 import itertools
-import threading
-from tkinter import ttk
 
 def select_file():
     file_path = filedialog.askopenfilename()
@@ -32,55 +30,42 @@ def crack_zip():
         messagebox.showerror("Error", "Please enter the maximum password length!")
     else:
         max_length = int(max_length)
+        found_password = None
 
-        progress_bar['value'] = 0  # Reset the progress bar
-        progress_label.config(text="Progress: 0%")
+        # Perform password cracking and ZIP file testing
+        with zipfile.ZipFile(file_path) as zf:
+            for length in range(1, max_length + 1):
+                # Generate all possible combinations based on the selected filter
+                if selected_filter == "Numbers":
+                    combinations = itertools.product("0123456789", repeat=length)
+                elif selected_filter == "Lowercase letters":
+                    combinations = itertools.product("abcdefghijklmnopqrstuvwxyz", repeat=length)
+                elif selected_filter == "Uppercase letters":
+                    combinations = itertools.product("ABCDEFGHIJKLMNOPQRSTUVWXYZ", repeat=length)
+                else:  # "All" filter
+                    combinations = itertools.product("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", repeat=length)
 
-        # Create a separate thread for password cracking
-        t = threading.Thread(target=perform_crack, args=(file_path, selected_filter, max_length))
-        t.start()
+                # Try each combination as a password
+                for combination in combinations:
+                    password = ''.join(combination)
+                    try:
+                        zf.extractall(pwd=password.encode())
+                        found_password = password
+                        break
+                    except Exception:
+                        pass
 
-def perform_crack(file_path, selected_filter, max_length):
-    found_password = None
-
-    # Perform password cracking and ZIP file testing
-    with zipfile.ZipFile(file_path) as zf:
-        total_combinations = 0
-        for length in range(1, max_length + 1):
-            total_combinations += len(selected_filter) ** length
-
-        progress = 0
-        for length in range(1, max_length + 1):
-            # Generate all possible combinations based on the selected filter
-            combinations = itertools.product(selected_filter, repeat=length)
-
-            # Try each combination as a password
-            for combination in combinations:
-                password = ''.join(combination)
-                try:
-                    zf.extractall(pwd=password.encode())
-                    found_password = password
+                if found_password:
                     break
-                except Exception:
-                    pass
 
-                progress += 1
-                progress_percentage = int((progress / total_combinations) * 100)
-                progress_bar['value'] = progress_percentage
-                progress_label.config(text=f"Progress: {progress_percentage}%")
-                window.update()
-
-            if found_password:
-                break
-
-    if found_password:
-        messagebox.showinfo("Cracking Complete", f"Password found: {found_password}")
-    else:
-        messagebox.showinfo("Cracking Complete", "No password found.")
+        if found_password:
+            messagebox.showinfo("Cracking Complete", f"Password found: {found_password}")
+        else:
+            messagebox.showinfo("Cracking Complete", "No password found.")
 
 window = tk.Tk()
 window.title("ZIP Password Cracker")
-window.geometry("500x350")
+window.geometry("500x300")
 window.configure(bg="#F8F8F8")
 
 header_label = tk.Label(window, text="ZIP Password Cracker", font=("Arial", 24, "bold"), bg="#F8F8F8")
@@ -104,11 +89,11 @@ filter_frame.pack()
 filter_label = tk.Label(filter_frame, text="Select filters:", font=("Arial", 14), bg="#F8F8F8")
 filter_label.pack(side=tk.LEFT)
 
-filters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+filters = ["Numbers", "Lowercase letters", "Uppercase letters", "All"]
 filter_var = tk.StringVar(window)
-filter_var.set(filters)  # Set all filters by default
+filter_var.set(filters[0])  # Set the default filter
 
-filter_dropdown = tk.OptionMenu(filter_frame, filter_var, filters)
+filter_dropdown = tk.OptionMenu(filter_frame, filter_var, *filters)
 filter_dropdown.config(font=("Arial", 12), bg="#4CAF50", fg="white")
 filter_dropdown.pack(side=tk.LEFT, padx=10)
 
@@ -123,11 +108,5 @@ length_entry.pack(side=tk.LEFT, padx=10)
 
 crack_button = tk.Button(window, text="Crack ZIP", command=crack_zip, font=("Arial", 16, "bold"), bg="#FF5722", fg="white")
 crack_button.pack(pady=20)
-
-progress_label = tk.Label(window, text="Progress: 0%", font=("Arial", 14), bg="#F8F8F8")
-progress_label.pack()
-
-progress_bar = ttk.Progressbar(window, orient=tk.HORIZONTAL, length=400, mode='determinate')
-progress_bar.pack(pady=10)
 
 window.mainloop()
